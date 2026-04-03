798a33a6a6ff189a36f8621a48ea0c54c591a154
Quang <vinhquangcontroller1@gmail.com>
2026-04-03 15:44:44 +0700

    Refactor vite.config.ts for cleaner configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,15 @@
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
-import {defineConfig, loadEnv} from 'vite';
+import tailwindcss from '@tailwindcss/vite';
 
-export default defineConfig(({mode}) => {
-  return {
-  base: '/Seedora/',  // 👈 THÊM DÒNG NÀY
+export default defineConfig({
+  base: '/Seedora/',
 
   plugins: [react(), tailwindcss()],
-  },
+
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, '.'),
+      '@': '/src',
     },
   },
-  server: {
-    hmr: process.env.DISABLE_HMR !== 'true',
-  },
-}
 });
