e1e17d506ba70879eb08b2c3b07465b4b1369c57
Quang <vinhquangcontroller1@gmail.com>
2026-04-03 15:42:05 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,10 @@ import path from 'path';
 import {defineConfig, loadEnv} from 'vite';
 
 export default defineConfig(({mode}) => {
-  const env = loadEnv(mode, '.', '');
   return {
   base: '/Seedora/',  // 👈 THÊM DÒNG NÀY
 
   plugins: [react(), tailwindcss()],
-  define: {
-    'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
   },
   resolve: {
     alias: {
